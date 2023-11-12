423de5052d717bc02e6a1c346eebe0809de7e605
DevNebulaP <76225632+DevNebulaP@users.noreply.github.com>
2023-11-12 14:50:12 +0700

    Fixed cross origin parameters

--- PaymentService/src/main/java/com/sopproject/paymentservice/controllers/PayPalPaymentController.java
+++ PaymentService/src/main/java/com/sopproject/paymentservice/controllers/PayPalPaymentController.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @RestController
-@CrossOrigin(origins = "", allowedHeaders = "")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PayPalPaymentController {
 
     @Autowired
--- PaymentService/src/main/java/com/sopproject/paymentservice/controllers/PremiumAccessController.java
+++ PaymentService/src/main/java/com/sopproject/paymentservice/controllers/PremiumAccessController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@CrossOrigin(origins = "", allowedHeaders = "")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class PremiumAccessController {
 
